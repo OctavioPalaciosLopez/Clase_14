8de1c917ed1015b778c7b68d81ded2c60bd6f005
OctavioPalacios <ocpl2710@gmail.com>
2019-09-20 21:20:53 -0500

    Actividad2

--- actividad2.js
+++ actividad2.js
@@ -4,11 +4,20 @@ const port = 3000
 
 const handleRoute = function (req, res) {
     res.send('Contestando una petición por Get')
-    }
+}
+const handleRoutePost = function (req, res) {
+    res.send('Contestando una petición por Post')
+}
+const handleRoutePut = function (req, res) {
+    res.send('Contestando una petición por Put')
+}
+const handleRouteDelete = function (req, res) {
+    res.send('Contestando una petición por Deleted')
+}
 app.get('/',handleRoute)
-app.post('/',handleRoute)
-app.put('/',handleRoute)
-app.delete('/',handleRoute)
+app.post('/',handleRoutePost)
+app.put('/',handleRoutePut)
+app.delete('/',handleRouteDelete)
 
 app.listen(port, function () { 
 console.log(`Example app listening on port ${port}!`)
